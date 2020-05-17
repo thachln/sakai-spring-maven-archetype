f413cc4d934204d27432ca83908a2020723a7eed
ThachLN <ThachLN@gmail.com>
2020-05-17 22:20:00 +0700

    Upgrade for Sakai 20.0

--- create-project.bat
+++ create-project.bat
@@ -1,1 +1,1 @@
-mvn archetype:generate -DarchetypeGroupId=org.sakaiproject.maven-archetype -DarchetypeArtifactId=sakai-spring-maven-archetype -DarchetypeVersion=19.3 -Dversion=19.3
+mvn archetype:generate -DarchetypeGroupId=org.sakaiproject.maven-archetype -DarchetypeArtifactId=sakai-spring-maven-archetype -DarchetypeVersion=20.0 -Dversion=20.0
