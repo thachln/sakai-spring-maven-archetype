66a140c250d24e102f64d285b333b72afff86774
ThachLN <thachln@gmail.com>
2020-10-15 09:11:35 +0700

    Upgrade code to support Sakai 22-SNAPSHOT

--- create-project.bat
+++ create-project.bat
@@ -1,1 +1,1 @@
-mvn archetype:generate -DarchetypeGroupId=org.sakaiproject.maven-archetype -DarchetypeArtifactId=sakai-spring-maven-archetype -DarchetypeVersion=20.1 -Dversion=20.1
+mvn archetype:generate -DarchetypeGroupId=org.sakaiproject.maven-archetype -DarchetypeArtifactId=sakai-spring-maven-archetype -DarchetypeVersion=22-SNAPSHOT -Dversion=22-SNAPSHOT
