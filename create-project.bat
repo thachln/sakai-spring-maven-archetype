0c870b2b178003641780d2648b0e6b9a91d915bb
thachln <thachln@gmail.com>
2023-03-25 16:38:56 +0700

    Support Sakai 22.2

--- create-project.bat
+++ create-project.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-mvn archetype:generate -DarchetypeGroupId=org.sakaiproject.maven-archetype -DarchetypeArtifactId=sakai-spring-maven-archetype -DarchetypeVersion=22.0 -Dversion=22.0
+mvn archetype:generate -DarchetypeGroupId=org.sakaiproject.maven-archetype -DarchetypeArtifactId=sakai-spring-maven-archetype -DarchetypeVersion=22.2 -Dversion=22.2
